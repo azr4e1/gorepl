e1301cd690526ad70b4d3985231ab150531b0ac0
Lorenzo Drumond <drumondlorenzo@posteo.net>
2026-02-09 16:12:48 +0000

    feat: added preliminary send cmd

--- cmd/sendCmd.go
+++ cmd/sendCmd.go
@@ -1,6 +1,11 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/azr4e1/gorepl/internals"
 	"github.com/spf13/cobra"
 )
 
@@ -13,7 +18,25 @@ var (
 	}
 )
 
-func Send(command *cobra.Command, args []string) {}
+func Send(command *cobra.Command, args []string) {
+	if len(args) == 0 {
+		command.Help()
+		os.Exit(0)
+	}
+	tempDirPath, err := internals.GetNPipePathCurDir()
+	if err != nil {
+		cobra.CheckErr(err)
+	}
+	nPipe, err := internals.NewTempFifo(tempDirPath)
+	if err != nil {
+		cobra.CheckErr(err)
+	}
+	cmd := []byte(fmt.Sprintln(strings.Join(args, " ")))
+	_, err = nPipe.Write(cmd)
+	if err != nil {
+		cobra.CheckErr(err)
+	}
+}
 
 func init() {
 	rootCmd.AddCommand(sendCmd)
