4cfb9bf0a89c870a1c22156593f37f00d2387a18
Lorenzo Drumond <drumondlorenzo@posteo.net>
2026-02-09 16:13:39 +0000

    feat: better separation of scope for named pipe: generate name, generate folder, retrieve name. Cwd scoped

--- internals/named_pipe.go
+++ internals/named_pipe.go
@@ -1,11 +1,15 @@
 package internals
 
 import (
-	"fmt"
-	"golang.org/x/sys/unix"
+	"crypto/sha256"
+	"encoding/hex"
 	"os"
 	"path"
+
+	"golang.org/x/sys/unix"
 )
+
+const NPipeName = "gorepl_named_pipe"
 
 type TempNPipe struct {
 	TempDir string
@@ -13,27 +17,64 @@ type TempNPipe struct {
 	fd      *os.File
 }
 
-func MkTempFifo(name string) (*TempNPipe, error) {
-	tempDir, err := os.MkdirTemp("", fmt.Sprintf("%s-*", name))
+func GetNPipePathCurDir() (string, error) {
+	curPath, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+	hash := sha256.New()
+	_, err = hash.Write([]byte(curPath))
+	if err != nil {
+		return "", err
+	}
+	res := hash.Sum([]byte("_gorepl"))
+	hexString := hex.EncodeToString(res)
+	tempDir := os.TempDir()
+
+	tempPath := path.Join(tempDir, hexString)
+	return tempPath, err
+}
+
+func MkTempFifo(tempDirPath string) (*TempNPipe, error) {
+	// check if dir exists; if so, remove it
+	if ok, _ := exists(tempDirPath); ok {
+		os.RemoveAll(tempDirPath)
+	}
+	err := os.Mkdir(tempDirPath, 0777)
 	if err != nil {
 		return nil, err
 	}
-	npipePath := path.Join(tempDir, fmt.Sprintf("%s-namedpipe", name))
+	npipePath := path.Join(tempDirPath, NPipeName)
 	err = unix.Mkfifo(npipePath, 0666)
 	if err != nil {
 		return nil, err
 	}
 	fd, err := os.OpenFile(npipePath, os.O_RDWR, 0)
 	if err != nil {
-		os.RemoveAll(tempDir)
+		os.RemoveAll(tempDirPath)
 		return nil, err
 	}
 
 	return &TempNPipe{
-		TempDir: tempDir,
+		TempDir: tempDirPath,
 		Name:    npipePath,
 		fd:      fd,
 	}, nil
+}
+
+func NewTempFifo(tempDirPath string) (*TempNPipe, error) {
+	npipePath := path.Join(tempDirPath, NPipeName)
+	fd, err := os.OpenFile(npipePath, os.O_RDWR, 0)
+	if err != nil {
+		return nil, err
+	}
+
+	return &TempNPipe{
+		TempDir: tempDirPath,
+		Name:    npipePath,
+		fd:      fd,
+	}, nil
+
 }
 
 func (tnp *TempNPipe) Read(p []byte) (int, error) {
