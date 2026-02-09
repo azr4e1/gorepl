d46441f035c84c9782657aa6d28dac23b9c39c09
Lorenzo Drumond <drumondlorenzo@posteo.net>
2026-02-09 16:15:01 +0000

    feat: added a Reader with echo to simplify multiplexer design

--- internals/utils.go
+++ internals/utils.go
@@ -1,9 +1,12 @@
 package internals
 
 import (
+	"errors"
 	"fmt"
 	"io"
+	"io/fs"
 	"log"
+	"os"
 	"sync"
 )
 
@@ -41,4 +44,44 @@ func NewLogger(w io.Writer, name string) *log.Logger {
 	logger := log.New(sw, fmt.Sprintf(sub, name), log.Ltime|log.Ldate|log.Lmsgprefix)
 
 	return logger
+}
+
+type ReaderWithEcho struct {
+	reader     io.ReadCloser
+	echoWriter io.Writer
+}
+
+func NewReaderWithEcho(reader io.ReadCloser, echo io.Writer) *ReaderWithEcho {
+	return &ReaderWithEcho{
+		reader:     reader,
+		echoWriter: echo,
+	}
+}
+
+func (re *ReaderWithEcho) Read(p []byte) (int, error) {
+	n, err := re.reader.Read(p)
+	if err != nil {
+		return n, err
+	}
+	_, err = re.echoWriter.Write(p[:n])
+
+	return n, err
+}
+
+func (re *ReaderWithEcho) Close() error {
+	return re.reader.Close()
+}
+
+// Source - https://stackoverflow.com/a/10510783
+// Posted by Mostafa, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-02-09, License - CC BY-SA 4.0
+func exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if errors.Is(err, fs.ErrNotExist) {
+		return false, nil
+	}
+	return false, err
 }
